2c29a0dcfcf614a3451f6e5b19ac4aa8bc475bd2
benisamuel <benisamuel566@gmail.com>
2025-04-09 19:26:02 +0200

    Added Password Hashing and jwt

--- src/main/java/com/employeedb/employeedb/service/AdminService.java
+++ src/main/java/com/employeedb/employeedb/service/AdminService.java
@@ -4,18 +4,22 @@ import com.employeedb.employeedb.dto.AdminDto;
 import com.employeedb.employeedb.exceptions.NotFoundException;
 import com.employeedb.employeedb.model.Admin;
 import com.employeedb.employeedb.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
     private final AdminRepository adminRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public AdminService ( AdminRepository adminRepository ) {
         this.adminRepository = adminRepository;
     }
 
     public Admin registerAdmin (AdminDto adminDto) {
-        Admin newAdmin = new Admin(adminDto.getName(), adminDto.getEmail(), adminDto.getPassword());
+        Admin newAdmin = new Admin(adminDto.getName(), adminDto.getEmail(), passwordEncoder.encode(adminDto.getPassword()));
         return this.adminRepository.save(newAdmin);
     }
 
--- src/main/java/com/employeedb/employeedb/service/AuthService.java
+++ src/main/java/com/employeedb/employeedb/service/AuthService.java
@@ -2,15 +2,19 @@ package com.employeedb.employeedb.service;
 
 import com.employeedb.employeedb.dto.AdminDto;
 import com.employeedb.employeedb.dto.AdminLoginDto;
+import com.employeedb.employeedb.exceptions.UnAuthorizedException;
 import com.employeedb.employeedb.model.Admin;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
     private final AdminService adminService;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService ( AdminService adminService ) {
+    public AuthService ( AdminService adminService, PasswordEncoder passwordEncoder ) {
         this.adminService = adminService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Admin registerAdmin (AdminDto adminDto) {
@@ -20,5 +24,8 @@ public class AuthService {
     public String loginAdmin (AdminLoginDto adminLoginDto) {
         Admin admin = this.adminService.getAdminByEmail(adminLoginDto.getEmail());
 
+        if (!passwordEncoder.matches(adminLoginDto.getPassword(),admin.getPassword())) {
+            throw new UnAuthorizedException("Invalid Credentials");
+        }
     }
 }
