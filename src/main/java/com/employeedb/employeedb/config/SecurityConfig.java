3c9ee35424799dab120b90d14d216ee1f9262ae1
benisamuel <benisamuel566@gmail.com>
2025-04-10 18:21:17 +0200

    Added Filters

--- src/main/java/com/employeedb/employeedb/config/SecurityConfig.java
+++ src/main/java/com/employeedb/employeedb/config/SecurityConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -14,14 +14,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("api/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+         return httpSecurity
+                 .authorizeHttpRequests(
+                         authorizeHttp -> {
+                             authorizeHttp.requestMatchers("/").permitAll();
+                             authorizeHttp.requestMatchers("/api/auth/login_admin").permitAll();
+                             authorizeHttp.requestMatchers("/api/auth/register-admin").permitAll();
+                             authorizeHttp.anyRequest().authenticated();
+                         }
+                 )
+                 .addFilterBefore(new SecurityFilter(), AuthenticationFilter.class)
+                 .build();
 
-        httpSecurity.addFilterBefore(jwt)
     }
 }
